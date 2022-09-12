8881ce6cb5067f0fb37010b1e5de0028a26597ff
elliotfayman <elliotfayman@gmail.com>
2022-09-11 23:34:33 -0700

    Adding js alert for mobile devises

--- js/mobile_checker.js
+++ js/mobile_checker.js
@@ -1,5 +1,9 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-		}
+	
+			function FP_popUpMsg(msg) {
+		alert(msg);
+			}
+}
 		else {
 			var h = document.getElementsByTagName('head').item(0);
 			var s = document.createElement("meta");
