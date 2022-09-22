cd0f1dcba4757dc9a6e5b56caa54d2fd0d1e2c35
elliotfayman <elliotfayman@gmail.com>
2022-09-22 16:50:28 -0700

    Fixing Animation error

--- js/animation.mjs
+++ js/animation.mjs
@@ -3,7 +3,7 @@ import { KawaseBlurFilter } from "https://cdn.skypack.dev/@pixi/filter-kawase-bl
 //import SimplexNoise from "https://cdn.skypack.dev/simplex-noise";
 import SimplexNoise from "https://cdn.skypack.dev/simplex-noise@4.0.0";
 import hsl from "https://cdn.skypack.dev/hsl-to-hex";
-import debounce from "https://cdn.skypack.dev/debounce";
+//import debounce from "https://cdn.skypack.dev/debounce";
 
 // return a random number within a range
 function random(min, max) {
