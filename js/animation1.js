ea96bce5c6ff00b409b3037a0a68eccf3f4caff3
elliotfayman <elliotfayman@gmail.com>
2022-09-23 12:26:26 -0700

    Fixing Animation error

--- js/animation1.js
+++ js/animation1.js
@@ -2,7 +2,7 @@
 import * as PIXI from "https://cdn.skypack.dev/pixi";
 import { KawaseBlurFilter } from "https://cdn.skypack.dev/@pixi/filter-kawase-blur";
 //import SimplexNoise from "https://cdn.skypack.dev/simplex-noise";
-import SimplexNoise from "https://cdn.skypack.dev/simplex-noise";
+import SimplexNoise from "https://cdn.skypack.dev/simplex-noise@3.0.0";
 import hsl from "https://cdn.skypack.dev/hsl-to-hex";
 import debounce from "https://cdn.skypack.dev/debounce";
 
