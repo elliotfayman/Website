1633075d82a3ba9dc1c2e3ba6cedc937f4e02006
elliotfayman <elliotfayman@gmail.com>
2023-01-16 23:27:10 -0800

    Fixing animation

--- js/animation.js
+++ js/animation.js
@@ -1,8 +1,6 @@
-
-import * as PIXI from "https://cdn.skypack.dev/pixi.js";
-import { KawaseBlurFilter } from "https://cdn.skypack.dev/@pixi/filter-kawase-blur";
-//import SimplexNoise from "https://cdn.skypack.dev/simplex-noise@3.0.0";
-import SimplexNoise from "https://cdn.skypack.dev/simplex-noise";
+import * as PIXI from "https://cdn.skypack.dev/pixi.js@5.x";
+import { KawaseBlurFilter } from "https://cdn.skypack.dev/@pixi/filter-kawase-blur@3.2.0";
+import SimplexNoise from "https://cdn.skypack.dev/simplex-noise@3.0.0";
 import hsl from "https://cdn.skypack.dev/hsl-to-hex";
 import debounce from "https://cdn.skypack.dev/debounce";
 
