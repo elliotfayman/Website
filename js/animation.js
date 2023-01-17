9ee625ffd256258536351d295f51bc4c75e64153
elliotfayman <elliotfayman@gmail.com>
2023-01-16 23:30:10 -0800

    Fixing animation

--- js/animation.js
+++ js/animation.js
@@ -11,7 +11,7 @@ function random(min, max) {
 
 // map a number from 1 range to another
 function map(n, start1, end1, start2, end2) {
-  return (n - start1) / (end1 - start1) * (end2 - start2) + start2;
+  return ((n - start1) / (end1 - start1)) * (end2 - start2) + start2;
 }
 
 // Create a new simplex noise instance
@@ -37,46 +37,46 @@ class ColorPalette {
     this.baseColor = hsl(this.hue, this.saturation, this.lightness);
     // define a complimentary color, 30 degress away from the base
     this.complimentaryColor1 = hsl(
-    this.complimentaryHue1,
-    this.saturation,
-    this.lightness);
-
+      this.complimentaryHue1,
+      this.saturation,
+      this.lightness
+    );
     // define a second complimentary color, 60 degrees away from the base
     this.complimentaryColor2 = hsl(
-    this.complimentaryHue2,
-    this.saturation,
-    this.lightness);
-
+      this.complimentaryHue2,
+      this.saturation,
+      this.lightness
+    );
 
     // store the color choices in an array so that a random one can be picked later
     this.colorChoices = [
-    this.baseColor,
-    this.complimentaryColor1,
-    this.complimentaryColor2];
-
+      this.baseColor,
+      this.complimentaryColor1,
+      this.complimentaryColor2
+    ];
   }
 
   randomColor() {
     // pick a random color
     return this.colorChoices[~~random(0, this.colorChoices.length)].replace(
-    "#",
-    "0x");
-
+      "#",
+      "0x"
+    );
   }
 
   setCustomProperties() {
     // set CSS custom properties so that the colors defined here can be used throughout the UI
     document.documentElement.style.setProperty("--hue", this.hue);
     document.documentElement.style.setProperty(
-    "--hue-complimentary1",
-    this.complimentaryHue1);
-
+      "--hue-complimentary1",
+      this.complimentaryHue1
+    );
     document.documentElement.style.setProperty(
-    "--hue-complimentary2",
-    this.complimentaryHue2);
-
-  }}
-
+      "--hue-complimentary2",
+      this.complimentaryHue2
+    );
+  }
+}
 
 // Orb class
 class Orb {
@@ -109,35 +109,35 @@ class Orb {
 
     // 250ms after the last window resize event, recalculate orb positions.
     window.addEventListener(
-    "resize",
-	
-    debounce(() => {
-      this.bounds = this.setBounds();
-    }, 250));
+      "resize",
+      debounce(() => {
+        this.bounds = this.setBounds();
+      }, 250)
+    );
   }
 
   setBounds() {
     // how far from the { x, y } origin can each orb move
     const maxDist =
-    window.innerWidth < 1000 ? window.innerWidth / 3 : window.innerWidth / 5;
+      window.innerWidth < 1000 ? window.innerWidth / 3 : window.innerWidth / 5;
     // the { x, y } origin for each orb (the bottom right of the screen)
     const originX = window.innerWidth / 1.25;
     const originY =
-    window.innerWidth < 1000 ?
-    window.innerHeight :
-    window.innerHeight / 1.375;
+      window.innerWidth < 1000
+        ? window.innerHeight
+        : window.innerHeight / 1.375;
 
     // allow each orb to move x distance away from it's x / y origin
     return {
       x: {
         min: originX - maxDist,
-        max: originX + maxDist },
-
+        max: originX + maxDist
+      },
       y: {
         min: originY - maxDist,
-        max: originY + maxDist } };
-
-
+        max: originY + maxDist
+      }
+    };
   }
 
   update() {
@@ -172,8 +172,8 @@ class Orb {
     this.graphics.drawCircle(0, 0, this.radius);
     // let graphics know we won't be filling in any more shapes
     this.graphics.endFill();
-  }}
-
+  }
+}
 
 // Create PixiJS app
 const app = new PIXI.Application({
@@ -182,18 +182,18 @@ const app = new PIXI.Application({
   // auto adjust size to fit the current window
   resizeTo: window,
   // transparent background, we will be creating a gradient background later using CSS
-  transparent: true });
+  transparent: true
+});
 
+app.stage.filters = [new KawaseBlurFilter(30, 10, true)];
 
 // Create colour palette
 const colorPalette = new ColorPalette();
 
-app.stage.filters = [new KawaseBlurFilter(30, 10, true)];
-
 // Create orbs
 const orbs = [];
 
-for (let i = 0; i < 10; i++) {if (window.CP.shouldStopExecution(0)) break;
+for (let i = 0; i < 10; i++) {
   const orb = new Orb(colorPalette.randomColor());
 
   app.stage.addChild(orb.graphics);
@@ -202,15 +202,15 @@ for (let i = 0; i < 10; i++) {if (window.CP.shouldStopExecution(0)) break;
 }
 
 // Animate!
-window.CP.exitedLoop(0);if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
   app.ticker.add(() => {
-    orbs.forEach(orb => {
+    orbs.forEach((orb) => {
       orb.update();
       orb.render();
     });
   });
 } else {
-  orbs.forEach(orb => {
+  orbs.forEach((orb) => {
     orb.update();
     orb.render();
   });
